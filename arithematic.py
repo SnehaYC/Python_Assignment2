39f933a6ac776270cc72b24959d66d09331219cc
snehachoudhari13 <62131585+snehachoudhari13@users.noreply.github.com>
2021-02-19 09:21:01 +0530

    Add files via upload

---
+++ arithematic.py
@@ -0,0 +1,20 @@
+#Defination of Addition function
+def Addition(no1,no2):
+    ans = no1 + no2
+    return ans
+ 
+#Defination of Substraction function
+def Substraction(no1,no2):
+    ans = no1 - no2
+    return ans
+ 
+#Defination of Multiplication function
+def Multiplication(no1,no2):
+    ans = no1 * no2
+    return ans
+    
+#Defination of Division function
+def Division(no1,no2):
+    ans = no1 / no2
+    return ans    
+ 
